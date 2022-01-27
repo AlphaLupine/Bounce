646f9ee2f63c46e19f98fcd6e6fd3edde198a897
Matthew <matthewduff0912@gmail.com>
2022-01-27 21:48:12 +0000

    Updated nodeConnect event to properly display host

--- src/events/erela/nodeConnect.ts
+++ src/events/erela/nodeConnect.ts
@@ -1,5 +1,5 @@
 import { ErelaEvent } from "../../lib/structures/Event";
 
 export default new ErelaEvent('nodeConnect', (node) => {
-    console.log(`Established connection with node ${node}`)
+    console.log(`Established connection with node ${node.options.host}`)
 })
