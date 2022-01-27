07e6d9bdb3538e51054d5084f7c466c8acd19239
Matthew <matthewduff0912@gmail.com>
2022-01-27 21:46:26 +0000

    Update - give client BounceClient type to access erela's manager

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,5 +1,8 @@
+import { BounceClient } from "@structures/BounceClient";
 import { ClientEvent } from "../../lib/structures/Event";
 
 export default new ClientEvent('ready', (client) => {
+    const bot = client as BounceClient;
+    bot.manager.init(client.user.id);
     console.log(`${client.user.username} is online`);
 })
