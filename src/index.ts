21b30b725611143ef85b90922369fdb2a5b27bab
Matthew <matthewduff0912@gmail.com>
2022-01-27 21:44:22 +0000

    Export added for root directory

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import { BounceEmbed } from './lib/structures/BounceEmbed';
 import { BounceClient } from './lib/structures/BounceClient';
 
+export const root = __dirname;
 export const client = new BounceClient();
 export const messageEmbed = BounceEmbed;
 client.start();
